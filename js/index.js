53300d7d7059e054196b9c8e6ae2844f014bd37a
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-06-12 20:39:44 +0200

    Add event handling for pop up form for creating new project card

--- js/index.js
+++ js/index.js
@@ -2,4 +2,8 @@ import includeHTML from "../js/htmlInjection.js";
 
 window.onload = function(){
     includeHTML();
+    document.getElementById("close").addEventListener("click", () => {document.getElementById("myModal").style.display = "none"})
+    document.getElementById("myModal").addEventListener("click", (event) => {event.target == document.getElementById("myModal")? event.target.style.display = "none":""})
 }
+
+
