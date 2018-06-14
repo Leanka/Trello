1becd647d1cba0980c6aedeaa8b6e6d0f098a895
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-06-14 20:12:16 +0200

    Resolve merge conflict

--- js/project_template.js
+++ js/project_template.js
@@ -1,7 +1,6 @@
 
 import * as include from "../js/htmlInjection.js";
 import * as models from "../js/models.js";
-var tasksIdCounter = 10;
 
 window.onload = function(){
     var projectKey = parseQuery(window.location.search);
@@ -129,12 +128,11 @@ function addNewTaskToList(ev) {
   ul.appendChild(li);
   ev.target.value = '';
   addRemoveListeners();
-  tasksIdCounter++;
 }
 
 function setLiAttributes(li, listId) {
   li.setAttribute("class", "task");
-  li.setAttribute("id", "task" + listId + tasksIdCounter);
+  li.setAttribute("id", "task" + listId + getCounter());
   li.setAttribute("draggable", "true");
   li.setAttribute("ondragstart", "drag(event)");
   li.insertAdjacentHTML('beforeend', '<span><i class="fa fa-trash"></i></span>');
