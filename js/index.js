9701a1207a2b188bb0ceb1ecfd75fd7118806b0b
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-06-28 15:41:52 +0200

    Create and fill onKeyPress(), add key press listner on form

--- js/index.js
+++ js/index.js
@@ -9,7 +9,17 @@ window.onload = function(){
     
     document.getElementById("close").addEventListener("click", () => {document.getElementById("myModal").style.display = "none"})
     document.getElementById("myModal").addEventListener("click", (event) => {event.target == document.getElementById("myModal")? event.target.style.display = "none":""})
+    document.getElementById("myModal").addEventListener('keypress', (event) => {onKeyPress(event, () => {getFormData()})})
     document.getElementById("modal-submit").addEventListener("click", () => {getFormData()})
+
+}
+
+function onKeyPress(event, callback){
+    let key = event.which || event.keyCode;
+    const enterKeyCode = 13;
+    if (key === enterKeyCode) {
+        callback()
+    }
 }
 
 function loadAllProjects(){
