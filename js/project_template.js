bf27f9f785622576dcd5430e41c04a4ddc6176eb
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-25 09:23:54 +0200

    Fix crossing over task

--- js/project_template.js
+++ js/project_template.js
@@ -119,14 +119,16 @@ function strikeTask(taskId){
     let task = taskContainer.getElementsByClassName("task-title")[0];
 
     let newStatus;
-    if(task.classList.contains("cross-over")){
-        task.classList.remove("cross-over");
-        newStatus = {"status":"todo"};
-    }else{
-        task.classList.add("cross-over");
-        newStatus = {"status":"done"};
-    }
-    tools.updateResource(taskId, "tasks", newStatus)
+    if(task.contentEditable == "false"){
+        if(task.classList.contains("cross-over")){
+            task.classList.remove("cross-over");
+            newStatus = {"status":"todo"};
+        }else{
+            task.classList.add("cross-over");
+            newStatus = {"status":"done"};
+        }
+        tools.updateResource(taskId, "tasks", newStatus)
+    }   
 }
 
 function setTrashSettings(container, itemId, removeResource, confirmation){
